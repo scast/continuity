e78ba61a70ba118afc654354d4d8eda474705982
Simón Castillo <simoncastillo@gmail.com>
2015-09-15 22:13:02 -0430

    adding a skeleton

---
+++ tests/test_continuity.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_continuity
+----------------------------------
+
+Tests for `continuity` module.
+"""
+
+import unittest
+
+from continuity import continuity
+
+
+class TestContinuity(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
+
+    def tearDown(self):
+        pass
